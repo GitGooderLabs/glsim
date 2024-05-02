d73189285baa6d9f3a2f5f99f74cf96110b634c2
GitGooderLabs <57209155+GitGooderLabs@users.noreply.github.com>
2024-05-02 16:22:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -122,7 +122,8 @@ server.get("/autogen/:file", async (req, res) => {
   try {
     const isFileExist = await checkIfFileExists(outputFile);
     if (!isFileExist) {
-      const output = await generateImage(fileName);
+/*8      const output = await generateImage(fileName); */
+      const output = await generateImage(`http://www.gooderlabs.com/sims/', fileName);
       if (output) return res.sendFile(outputFile);
       return res.send("OH NO!!!  Something went wrong!!!");
     }
